be775a83cb3f88cada923944e2e8035f02e650e1
BojanV03 <bvasiljevic95@gmail.com>
2017-10-26 16:22:45 +0200

    Fixed death timing and added a small delay for better user experience

--- main.c
+++ main.c
@@ -15,43 +15,44 @@ void render(void);
 void tick(int id);
 
 int main(int argc, char* argv[]) {
-  
+
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
-  
+
   glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
   glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-WINDOW_WIDTH) / 2,
                      (glutGet(GLUT_SCREEN_HEIGHT)-WINDOW_HEIGHT)/2);
   glutCreateWindow("Flappy Quad v1.0");
-  
+
   srand(time(NULL));
-  
+
   glutKeyboardFunc(input);
   glutDisplayFunc(render);
   glutTimerFunc(TIMER_INTERVAL, tick, TIMER_ID);
-  
+
   /* player initialization */
   player.size = 20;
   player.x = WINDOW_WIDTH/2-player.size/2;
   player.y = WINDOW_HEIGHT/2-player.size/2;
   player.yvel = player.xvel = 0;
   player.jump = -6.5;
-  
-  
+  player.alive = 1;
+
+
   /* pipes initialization */
   pair[0] = create_pair(300, 0, 50, 1.5);
   pair[1] = create_pair(pair[0].p1.x+PIPE_DISTANCE, 0, 50, 1.5);
-  
-  
+
+
   glClearColor(1, 1, 1, 0);
   glEnable(GL_DEPTH_TEST);
-  
+
   glutMainLoop();
-  
+
   return 0;
 }
 void input(unsigned char key, int x, int y) {
-  
+
   switch (key) {
     case 27:
       /* escape character - exit game */
@@ -65,57 +66,44 @@ void input(unsigned char key, int x, int y) {
 }
 
 void render(void) {
-  
+
   int i;
-  
+
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   glOrtho(0.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 0.0f, 0.0f, 1.0f);
-  
+
   render_player(&player);
   for (i = 0; i < 2; i++)
     render_pair(&pair[i]);
-  
-  
-  
-  
+
+
+
+
   glutSwapBuffers();
 }
 void tick(int id) {
-  
-  int i;
-  
-  if (id != TIMER_ID) return;
-  
-  update_player(&player);
-  for (i = 0; i < 2; i++) {
-    update_pair(&pair[i],pair[(i+1)%2].p1.x);
-    if (collision_pair(&player, &pair[i]))
-      exit(1);
+  if(player.alive) {
+    int i;
+
+    if (id != TIMER_ID) return;
+
+    update_player(&player);
+    for (i = 0; i < 2; i++) {
+      update_pair(&pair[i],pair[(i+1)%2].p1.x);
+      if (collision_pair(&player, &pair[i]))
+        player.alive = 0;
+    }
   }
-  
-  
+  else {
+    printf("%.2f\n", dead_for);
+    dead_for += glutGet(GLUT_ELAPSED_TIME);
+    if (dead_for > 200000) // 2 seconds
+    {
+      exit(1);
+    }
+  }
   glutPostRedisplay();
   glutTimerFunc(TIMER_INTERVAL, tick, TIMER_ID);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
